localalignementkernel overflow + linalg error fixed

src/decomposition/KernelPCA.py
@@ -69,10 +69,12 @@ class KernelPCA:
 
         # Compute explained variance
         _, S, __ = np.linalg.svd(gram_matrix, full_matrices=False)
-        self._explained_variance = (S ** 2) / (n - 1)
-        total_var = self._explained_variance.sum()
+        _explained_variance = (S ** 2) / (n - 1)
+        self._explained_variance = _explained_variance[:self.n_components]
+        total_var = _explained_variance.sum()
         self._explained_variance_ratio = self._explained_variance / total_var
-        self._singular_values = S.copy()
+        _singular_values = S.copy()
+        self._singular_values = _singular_values[:self.n_components]
 
         # Mark as fitted
         self._fitted = True

utils/DataLoader.py
@@ -19,12 +19,8 @@ class DataLoader:
         return self._data_dir
 
     def load(self, dataset_id, as_array=False):
-        path = os.path.join(self.data_dir, dataset_id)
-        if as_array:
-            output = np.loadtxt(path)
-        else:
-            output = pd.read_csv(path, index_col=0)
-        return output
+        df = pd.read_csv(os.path.join(self.data_dir, dataset_id), index_col=0)
+        return df.values.squeeze() if as_array else df
 
     @staticmethod
     def train_val_split(x, y, val_size=0.33, random_state=42):